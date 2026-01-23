0402e5e2fca7ff5cda37448bd777ded2e6453ea8
Ľubomír Samotný <lubomir.samotny@smartbase.sk>
2026-01-23 17:51:52 +0100

    triv: fix refund response model fields

--- src/models/RefundResponse.ts
+++ src/models/RefundResponse.ts
@@ -3,24 +3,15 @@
  * @category Model
  */
 export class RefundResponse {
-  refundId: string = '';
-  status: string = '';
-  amount: number = 0.0;
-  currency: string = '';
+  transactionId: string = '';
 
   constructor() {
-    this.refundId = '';
-    this.status = '';
-    this.amount = 0.0;
-    this.currency = '';
+    this.transactionId = '';
   }
 
   static fromObject(data: any = {}): RefundResponse {
     const instance = new RefundResponse();
-    instance.refundId = data.refundId || '';
-    instance.status = data.status || '';
-    instance.amount = data.amount || 0.0;
-    instance.currency = data.currency || '';
+    instance.transactionId = data.transactionId || '';
     return instance;
   }
 }
